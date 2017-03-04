3ce78ec93f6d84c2a8591d4c3f469d2ab8321c68
Rafal Ganczarek <github@ganczarek.info>
2017-03-04 14:19:35 +0000

    Initial implementation of commit creation

--- src/gitclient/gitclient.go
+++ src/gitclient/gitclient.go
@@ -4,19 +4,35 @@ import (
 	"os"
 	"github.com/libgit2/git2go"
 	"fmt"
+	"time"
+	"io/ioutil"
 )
 
 type gitClient struct {
 	repoName       string
 	parentRepoPath string
+	authorName     string
+	authorEmail    string
 }
 
-func GitClient(repoName string, repoPath string) gitClient {
-	return gitClient{repoName, repoPath}
+func GitClient(repoName string, repoPath string, config *git.Config) (*gitClient, error) {
+	authorEmail, err := config.LookupString("user.email")
+	if err != nil {
+		return nil, err
+	}
+	authorName, err := config.LookupString("user.name")
+	if err != nil {
+		return nil, err
+	}
+	return &gitClient{repoName, repoPath, authorName, authorEmail}, nil
 }
 
 func (gc gitClient) RepoConfigPath() string {
-	return fmt.Sprintf("%s/%s/.git", gc.parentRepoPath, gc.repoName)
+	return fmt.Sprintf("%s/.git", gc.RepoPath())
+}
+
+func (gc gitClient) RepoPath() string {
+	return fmt.Sprintf("%s/%s", gc.parentRepoPath, gc.repoName)
 }
 
 func (gc gitClient) InitRepo() error {
@@ -24,6 +40,41 @@ func (gc gitClient) InitRepo() error {
 	if err != nil {
 		return err
 	}
-	git.InitRepository(gc.RepoConfigPath(), true)
+	_, err = git.InitRepository(gc.RepoConfigPath(), false)
+	return err
+}
+
+func (gc gitClient) CreateCommitAtDate(commitDate time.Time, commitMessage string) error {
+	tmpfile := "README"
+	err := ioutil.WriteFile(gc.RepoPath() + "/" + tmpfile, []byte("foo\n"), 0644)
+	repo, err := git.OpenRepository(gc.RepoConfigPath())
+	if err != nil {
+		return err
+	}
+	sig := &git.Signature{Name: gc.authorName, Email: gc.authorEmail, When: commitDate}
+	idx, err := repo.Index()
+	err = idx.AddByPath("README")
+	if err != nil {
+		return err
+	}
+
+	err = idx.Write()
+	if err != nil {
+		return err
+	}
+
+	treeId, err := idx.WriteTree()
+	if err != nil {
+		return err
+	}
+
+	tree, err := repo.LookupTree(treeId)
+	if err != nil {
+		return err
+	}
+	_, err = repo.CreateCommit("HEAD", sig, sig, commitMessage, tree)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- src/gitclient/gitclient_test.go
+++ src/gitclient/gitclient_test.go
@@ -7,28 +7,79 @@ import (
 	"fmt"
 	"os"
 	"github.com/stretchr/testify/assert"
+	"time"
+	"github.com/libgit2/git2go"
 )
 
-func Test_ShouldReturnErrorIfCannotCreateRepo(t *testing.T) {
+func Test_InitRepo_ShouldReturnErrorIfCannotCreateRepo(t *testing.T) {
 	repoName := "IncorrectTestRepo"
 	// should not e able to touch root home dir
+	config := setupConfig(t)
+	defer cleanupConfig()
 	incorrectPath := fmt.Sprintf("/root/go_gitclient_test/%s", uuid.New().String())
+	gitClient, err := GitClient(repoName, incorrectPath, config)
+	if err != nil {
+		t.Fatal("Failed to create git client!")
+	}
 
-	gitClient := GitClient(repoName, incorrectPath)
 	gitClient.InitRepo()
 
-	_, err := os.Stat(fmt.Sprintf("%s/%s/.git", incorrectPath, repoName))
+	_, err = os.Stat(fmt.Sprintf("%s/%s/.git", incorrectPath, repoName))
 	assert.NotNil(t, err)
 	assert.True(t, os.IsNotExist(err))
 }
 
-func Test_ShouldCreateANewRepo(t *testing.T) {
+func Test_InitRepo_ShouldCreateANewRepo(t *testing.T) {
 	repoName := "TestRepo"
+	config := setupConfig(t)
+	defer cleanupConfig()
 	repoPath := fmt.Sprintf("%s/go_gitclient_test/%s", os.TempDir(), uuid.New().String())
+	gitClient, err := GitClient(repoName, repoPath, config)
+	if err != nil {
+		t.Fatal("Failed to create git client!")
+	}
 
-	gitClient := GitClient(repoName, repoPath)
 	gitClient.InitRepo()
 
-	_, err := os.Stat(fmt.Sprintf("%s/%s/.git", repoPath, repoName))
+	_, err = os.Stat(fmt.Sprintf("%s/%s/.git", repoPath, repoName))
 	assert.Nil(t, err)
+}
+
+func Test_Commit_ShouldNotReturnErrorWhenCreateCommitInThePast(t *testing.T) {
+	repoName := "TestRepo"
+	config := setupConfig(t)
+	defer cleanupConfig()
+	repoPath := fmt.Sprintf("%s/go_gitclient_test/%s", os.TempDir(), uuid.New().String())
+	gitClient, err := GitClient(repoName, repoPath, config)
+	if err != nil {
+		t.Fatal("Failed to create git client!")
+	}
+	date := time.Date(2016, time.June, 6, 12, 0, 0, 0, time.UTC)
+
+	gitClient.InitRepo()
+	err = gitClient.CreateCommitAtDate(date, "Test commit message")
+	assert.Nil(t, err)
+}
+
+var tempConfig = os.TempDir() + "/temp.gitconfig"
+
+func setupConfig(t *testing.T) *git.Config {
+	c, err := git.OpenOndisk(nil, tempConfig)
+	if err != nil {
+		t.Fatalf("Failed to open %s", tempConfig)
+	}
+
+	err = c.SetString("user.name", "Test User Name")
+	if err != nil {
+		t.Fatal("Failed to set user.name value in test config")
+	}
+	err = c.SetString("user.email", "user@test.com")
+	if err != nil {
+		t.Fatal("Failed to set user.email value in test config")
+	}
+	return c
+}
+
+func cleanupConfig() {
+	os.Remove(tempConfig)
 }
