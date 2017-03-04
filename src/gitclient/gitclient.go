4c9618746b8b177a2c78297a7bc58778b677d9ec
Rafal Ganczarek <github@ganczarek.info>
2017-03-04 15:41:11 +0000

    Refactored git client to hold a pointer to repository

--- src/gitclient/gitclient.go
+++ src/gitclient/gitclient.go
@@ -8,13 +8,14 @@ import (
 	"io/ioutil"
 )
 
-type gitClient struct {
+type GitClient struct {
 	RepoPath  string
 	UserName  string
 	UserEmail string
+	Repo      *git.Repository
 }
 
-func GitClient(repoPath string, config *git.Config) (*gitClient, error) {
+func New(repoPath string, config *git.Config) (*GitClient, error) {
 	authorEmail, err := config.LookupString("user.email")
 	if err != nil {
 		return nil, err
@@ -23,23 +24,26 @@ func GitClient(repoPath string, config *git.Config) (*gitClient, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &gitClient{repoPath, authorName, authorEmail}, nil
+	repository, err := initRepo(repoPath)
+	if err != nil {
+		return nil, err
+	}
+	return &GitClient{repoPath, authorName, authorEmail, repository}, nil
 }
 
-func (gc gitClient) RepoConfigPath() string {
+func initRepo(repoPath string) (*git.Repository, error) {
+	err := os.MkdirAll(repoPath, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	return git.InitRepository(repoPath, false)
+}
+
+func (gc GitClient) RepoConfigPath() string {
 	return fmt.Sprintf("%s/.git", gc.RepoPath)
 }
 
-func (gc gitClient) InitRepo() error {
-	err := os.MkdirAll(gc.RepoConfigPath(), os.ModePerm)
-	if err != nil {
-		return err
-	}
-	_, err = git.InitRepository(gc.RepoConfigPath(), false)
-	return err
-}
-
-func (gc gitClient) CreateCommitAtDate(commitDate time.Time, commitMessage string) error {
+func (gc GitClient) CreateCommitAtDate(commitDate time.Time, commitMessage string) error {
 	tmpfile := "README"
 	err := ioutil.WriteFile(gc.RepoPath + "/" + tmpfile, []byte("foo\n"), 0644)
 	repo, err := git.OpenRepository(gc.RepoConfigPath())
--- src/gitclient/gitclient_test.go
+++ src/gitclient/gitclient_test.go
@@ -11,93 +11,108 @@ import (
 	"github.com/libgit2/git2go"
 )
 
-func Test_ClientConstruction_ShouldFailIfNoConfigFile(t *testing.T) {
+const TEST_USER_NAME = "Test User Name"
+const TEST_USER_EMAIL = "user@test.com"
+
+var TEST_TEMP_DIR = os.TempDir() + "/go_gitclient_test"
+var TEST_TEMP_CONFIG = TEST_TEMP_DIR + "/temp_test.gitconfig"
+
+func Test_ClientConstruction_ShouldFail_WhenNoConfigFile(t *testing.T) {
 	repoPath := fmt.Sprintf("%s/go_gitclient_test/%s", os.TempDir(), uuid.New().String())
 	config, err := git.OpenOndisk(nil, "non-esiting-git-config-file")
-	_, err = GitClient(repoPath, config)
+
+	_, err = New(repoPath, config)
+
 	assert.NotNil(t, err)
 }
 
 func Test_ClientConstruction_WhenConfigFileExists(t *testing.T) {
 	repoPath := fmt.Sprintf("%s/go_gitclient_test/%s", os.TempDir(), uuid.New().String())
-	config := setupConfig(t)
-	defer cleanupConfig()
-	gitClient, err := GitClient(repoPath, config)
+	config := setupTestConfig(t)
+	defer cleanupTestConfig()
+
+	gitClient, err := New(repoPath, config)
+
 	assert.Nil(t, err)
 	assert.Equal(t, gitClient.UserName, TEST_USER_NAME)
 	assert.Equal(t, gitClient.UserEmail, TEST_USER_EMAIL)
 }
 
-func Test_InitRepo_ShouldReturnErrorIfCannotCreateRepo(t *testing.T) {
-	repoName := "IncorrectTestRepo"
+func Test_ClientConstruction_ShouldReturnErrorIfCannotCreateRepo(t *testing.T) {
+	config := setupTestConfig(t)
+	defer cleanupTestConfig()
 	// should not e able to touch root home dir
-	config := setupConfig(t)
-	defer cleanupConfig()
 	incorrectPath := fmt.Sprintf("/root/go_gitclient_test/%s", uuid.New().String())
-	gitClient, err := GitClient(incorrectPath, config)
-	if err != nil {
-		t.Fatal("Failed to create git client!")
-	}
 
-	gitClient.InitRepo()
+	_, err := New(incorrectPath, config)
 
-	_, err = os.Stat(fmt.Sprintf("%s/%s/.git", incorrectPath, repoName))
+	assert.NotNil(t, err)
+	_, err = os.Stat(fmt.Sprintf("%s/.git", incorrectPath))
 	assert.NotNil(t, err)
 	assert.True(t, os.IsNotExist(err))
 }
 
 func Test_InitRepo_ShouldCreateANewRepo(t *testing.T) {
-	repoName := "TestRepo"
-	config := setupConfig(t)
-	defer cleanupConfig()
-	repoPath := fmt.Sprintf("%s/go_gitclient_test/%s/%s", os.TempDir(), uuid.New().String(), repoName)
-	gitClient, err := GitClient(repoPath, config)
-	if err != nil {
-		t.Fatal("Failed to create git client!")
-	}
+	gitClient := setupTestRepoAndClient(t)
+	defer cleanupTestRepo()
 
-	gitClient.InitRepo()
-
-	_, err = os.Stat(fmt.Sprintf("%s/%s/.git", repoPath, repoName))
+	_, err := os.Stat(fmt.Sprintf("%s/.git", gitClient.RepoPath))
 	assert.Nil(t, err)
 }
 
 func Test_Commit_ShouldNotReturnErrorWhenCreateCommitInThePast(t *testing.T) {
-	repoName := "TestRepo"
-	config := setupConfig(t)
-	defer cleanupConfig()
-	repoPath := fmt.Sprintf("%s/go_gitclient_test/%s/%s", os.TempDir(), uuid.New().String(), repoName)
-	gitClient, err := GitClient(repoPath, config)
-	if err != nil {
-		t.Fatal("Failed to create git client!")
-	}
+	gitClient := setupTestRepoAndClient(t)
+	defer cleanupTestRepo()
 	date := time.Date(2016, time.June, 6, 12, 0, 0, 0, time.UTC)
 
-	gitClient.InitRepo()
+	// repository should be empty before commit
+	isRepoEmpty, err := gitClient.Repo.IsEmpty()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.True(t, isRepoEmpty)
+
 	err = gitClient.CreateCommitAtDate(date, "Test commit message")
+
 	assert.Nil(t, err)
+	isRepoEmpty, err = gitClient.Repo.IsEmpty()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.False(t, isRepoEmpty)
 }
 
-var TEMP_TEST_CONFIG = os.TempDir() + "/temp_test.gitconfig"
-const TEST_USER_NAME = "Test User Name"
-const TEST_USER_EMAIL = "user@test.com"
-
-func setupConfig(t *testing.T) *git.Config {
-	c, err := git.OpenOndisk(nil, TEMP_TEST_CONFIG)
+func setupTestConfig(t *testing.T) *git.Config {
+	c, err := git.OpenOndisk(nil, TEST_TEMP_CONFIG)
 	if err != nil {
-		t.Fatalf("Failed to open %s", TEMP_TEST_CONFIG)
+		t.Fatalf("Failed to open %s", TEST_TEMP_CONFIG)
 	}
 	err = c.SetString("user.name", TEST_USER_NAME)
 	if err != nil {
-		t.Fatalf("Failed to set user.name value in %d", TEMP_TEST_CONFIG)
+		t.Fatalf("Failed to set user.name value in %d", TEST_TEMP_CONFIG)
 	}
 	err = c.SetString("user.email", TEST_USER_EMAIL)
 	if err != nil {
-		t.Fatalf("Failed to set user.email value in %s", TEMP_TEST_CONFIG)
+		t.Fatalf("Failed to set user.email value in %s", TEST_TEMP_CONFIG)
 	}
 	return c
 }
 
-func cleanupConfig() {
-	os.Remove(TEMP_TEST_CONFIG)
+func cleanupTestConfig() {
+	os.Remove(TEST_TEMP_CONFIG)
+}
+
+func setupTestRepoAndClient(t *testing.T) *GitClient {
+	config := setupTestConfig(t)
+	repoPath := fmt.Sprintf("%s/%s/%s", TEST_TEMP_DIR, uuid.New().String(), "TestRepo")
+	gitClient, err := New(repoPath, config)
+	if err != nil {
+		t.Fatalf("Failed to create git client!\n%s", err)
+	}
+	return gitClient
+}
+
+func cleanupTestRepo() {
+	cleanupTestConfig()
+	os.Remove(TEST_TEMP_DIR)
 }
