2978232206506692d540f1d6a71baa0c0bebf56f
Rafal Ganczarek <github@ganczarek.info>
2017-03-04 18:13:47 +0000

    Calculate commit time with an offset

--- src/app/GithubAsciiArtApp_test.go
+++ src/app/GithubAsciiArtApp_test.go
@@ -16,3 +16,13 @@ func Test_ShouldCalculateTimeForFirstMondayOfAYear(t *testing.T) {
 
 	assert.Equal(t, expectedTime, result)
 }
+
+func Test_ShouldCalculateTimeWithOffset(t *testing.T) {
+	commitData := reader.CommitData{0, 0, 1}
+	offset := 5
+	expectedTime := time.Date(2014, 0, 7 * offset, 12, 0, 0, 0, time.UTC)
+
+	result := CalculateCommitTime(commitData, 2014, offset)
+
+	assert.Equal(t, expectedTime, result)
+}
